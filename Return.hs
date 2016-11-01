8cc4098bcc336911f09870999d475b333fba90da
reinvdwoerd <reinvanderwoerd@me.com>
2016-11-01 10:34:02 +0100

    IO

---
+++ Return.hs
@@ -0,0 +1,10 @@
+module Main where
+
+main :: IO ()
+main = do
+    return ()
+    return "HAHAHA"
+    line <- getLine
+    return "BLAH BLAH BLAH"
+    return 4
+    putStrLn line
--- ReverseWords.hs
+++ ReverseWords.hs
@@ -1,17 +1,12 @@
 module Main where
 
-infixr 0 |>
 
-(|>) :: a -> (a -> b) -> b
-(|>) = flip ($)
-
-main :: IO ()
 main = do
   line <- getLine
   if null line
     then return ()
     else do
-      print reverseWords line
+      print $ reverseWords line
       main
 
 reverseWords :: String -> String
