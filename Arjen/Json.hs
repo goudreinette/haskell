c26f6d82dd6e6e4b6da178b8fac45fd997807caf
reinvdwoerd <reinvanderwoerd@me.com>
2016-11-14 15:54:25 +0100

    JSON

---
+++ Arjen/Json.hs
@@ -0,0 +1,55 @@
+module Json where
+
+import           Data.List.Split
+import           Flow
+
+data Expression
+  = Expression String Value
+  deriving (Eq, Show)
+
+data Value
+  = Float
+  | String
+  | Bool
+  | JsonObject
+  | JsonArray
+  deriving (Eq, Show)
+
+data Token
+  = ObjectStart
+  | ObjectEnd
+  | ArrayStart
+  | ArrayEnd
+  | Quote
+  | Colon
+  | Null
+  | True'
+  | False'
+  | Any String
+  deriving (Eq, Show)
+
+
+tokenize string =
+  tokens
+  where
+    tokens = split' string |> filter (/= "") |> map match
+
+split' string = do
+  lines string
+  >>= words
+  >>= split (dropInnerBlanks (oneOf "\",{}:[]"))
+  >>= splitOn "null"
+  >>= splitOn "true"
+  >>= splitOn "false"
+
+match char =
+  case char of
+    "\""    -> Quote
+    "{"     -> ObjectStart
+    "}"     -> ObjectEnd
+    "["     -> ArrayStart
+    ":"     -> Colon
+    "]"     -> ArrayEnd
+    "true"  -> True'
+    "false" -> False'
+    _       -> Any char
--- rekenen/Fractional.hs
+++ rekenen/Fractional.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Fractional where
 
-type Numerator = Integer
+type Numerator   = Integer
 type Denominator = Integer
-type Fraction = (Numerator, Denominator)
+type Fraction    = (Numerator, Denominator)
 
 instance Num Fraction where
   negate (num, denom) = (-num, denom)
+  (-)                 = manipulate (-)
   (+)                 = manipulate (+)
   (*)                 = manipulate (*)
   fromInteger num     = (num, 1)
   abs (num, denom)    = (abs num, denom)
+  signum (num, denom)
+    | num < 0  = -1
+    | num == 0 = 0
+    | num > 0  = 1
+
 
 manipulate f (num, denom) (num', denom') =
   simplify (numerator, denominator) where
