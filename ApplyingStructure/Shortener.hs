e94d4b69b57bc910615528ca4f266a7b1929efdc
reinvdwoerd <reinvanderwoerd@me.com>
2016-11-15 18:06:57 +0100

    Shortener

--- ApplyingStructure/Shortener.hs
+++ ApplyingStructure/Shortener.hs
@@ -1,1 +1,17 @@
-module Shortener
+module Shortener where
+
+import           Control.Monad
+import           Flow
+import qualified System.Random as Random
+
+alphaNum :: String
+alphaNum = ['a'..'z'] ++ ['0'..'9']
+
+randomElement :: String -> IO Char
+randomElement xs = do
+  let maxIndex = length xs - 1
+  randomDigit <- Random.randomRIO (0, maxIndex)
+  return (xs !! randomDigit)
+
+shortyGen :: IO String
+shortyGen = replicateM 7 (randomElement alphaNum)
