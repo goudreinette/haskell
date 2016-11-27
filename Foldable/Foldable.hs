d67b585a5cf6e5b1a6ae4493ed8c6c22d084c2c3
reinvdwoerd <reinvanderwoerd@me.com>
2016-11-27 19:24:21 +0100

    --

--- Foldable/Foldable.hs
+++ Foldable/Foldable.hs
@@ -1,3 +1,11 @@
 module Foldable where
 
-import           Data.Monoid
+import           Data.Foldable
+
+data Identity a = Identity a
+
+
+instance Foldable Identity where
+  foldr f z (Identity x) = f x z
+  foldl f z (Identity x) = f z x
+  foldMap f (Identity x) = f x
