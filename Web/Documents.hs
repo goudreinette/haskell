736b08b1b5a962815d25ee3cfda9d1976ac1162a
reinvdwoerd <reinvanderwoerd@me.com>
2016-12-13 11:51:01 +0100

    Typesafe document store

--- Web/Documents.hs
+++ Web/Documents.hs
@@ -1,14 +1,45 @@
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Documents where
 
-import Data.Aeson hiding (json)
+import Control.Monad.Trans
+import Data.Aeson              hiding (json)
 import Data.IORef
 import Data.Map
+import Data.String.Conversions
+import Data.Text.Conversions
+import GHC.Generics
 import Web.Scotty
 
 
-resource :: (ToJSON a, FromJSON a) => String -> a -> IORef (Map a a) -> ActionM
-resource name schema ref = do
-  get ("/:" ++ name) $ do
-    all <- liftIO $ readIORef ref
-    json all
+data Person = Person
+  { firstName :: String
+  , lastName  :: String
+  , age       :: Int
+  } deriving (Generic, FromJSON, ToJSON)
+
+main = do
+  store <- newIORef (empty :: Map Int Person)
+  scotty 3000 $ do
+    get "/people" $ do
+      people <- getPeople store
+      json people
+    post "/person/" $ do
+      body <- body
+      people <- getPeople store
+      let id'     = size people
+          result = eitherDecode body :: Either String Person
+
+      case result of
+        Left error   -> do
+          text $ convertText error
+        Right person -> do
+          liftIO $ modifyIORef' store (insert id' person)
+          json person
+
+
+
+
+getPeople store =
+  liftIO $ readIORef store
